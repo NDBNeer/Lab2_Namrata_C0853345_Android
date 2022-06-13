ed56c2866a8bc32cf252ba658e1949d41f727663
Namrata Barot <namrata.barot3@gmail.com>
2022-06-13 17:52:53 -0400

    Creating model to get and set values of products

---
+++ app/src/main/java/com/lambton/lab2_namrata_c0853345_android/model/Products.java
@@ -0,0 +1,46 @@
+package com.lambton.lab2_namrata_c0853345_android.model;
+
+public class Products {
+    int product_id;
+    String product_name, product_description;
+    double product_price;
+
+    public Products(int product_id, String product_name, String product_description, double product_price) {
+        this.product_id = product_id;
+        this.product_name = product_name;
+        this.product_description = product_description;
+        this.product_price = product_price;
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public String getProduct_description() {
+        return product_description;
+    }
+
+    public void setProduct_description(String product_description) {
+        this.product_description = product_description;
+    }
+
+    public double getProduct_price() {
+        return product_price;
+    }
+
+    public void setProduct_price(double product_price) {
+        this.product_price = product_price;
+    }
+}
