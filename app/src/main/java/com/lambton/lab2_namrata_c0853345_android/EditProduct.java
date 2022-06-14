674a993036a191dee8977a53a70221054ccfbc91
Namrata Barot <namrata.barot3@gmail.com>
2022-06-13 21:39:13 -0400

    Created Edit Product and ProductDetailAdapter

---
+++ app/src/main/java/com/lambton/lab2_namrata_c0853345_android/EditProduct.java
@@ -0,0 +1,51 @@
+package com.lambton.lab2_namrata_c0853345_android;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.lambton.lab2_namrata_c0853345_android.R;
+import com.lambton.lab2_namrata_c0853345_android.sqlite.DbAdapter;
+
+public class EditProduct extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnAddToDb;
+    EditText etProductName, etProductDescription, etProductPrice;
+    DbAdapter dbAdapter = new DbAdapter(this);
+    String prod_id,prod_name,prod_desc,prod_price;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_product);
+        findId();
+        Intent i=getIntent();
+        prod_id = i.getStringExtra("prod_id");
+        prod_name=i.getStringExtra("prod_name");
+        prod_desc = i.getStringExtra("prod_desc");
+        prod_price= i.getStringExtra("prod_price");
+        etProductName.setText(prod_name);
+        etProductDescription.setText(prod_desc);
+        etProductPrice.setText(prod_price);
+    }
+    private void findId(){
+        etProductName = findViewById(R.id.product_name);
+        etProductDescription = findViewById(R.id.product_description);
+        etProductPrice = findViewById(R.id.product_price);
+        btnAddToDb = findViewById(R.id.btn_add);
+        btnAddToDb.setText("Edit Product");
+        btnAddToDb.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn_add){
+            dbAdapter.updateProduct(Integer.parseInt(prod_id),etProductName.getText().toString(),
+                    etProductDescription.getText().toString(),
+                    Double.parseDouble(etProductPrice.getText().toString()));
+        }
+    }
+}
---
+++ app/src/main/java/com/lambton/lab2_namrata_c0853345_android/adapter/ProductDetailAdapter.java
@@ -0,0 +1,75 @@
+package com.lambton.lab2_namrata_c0853345_android.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.lambton.lab2_namrata_c0853345_android.AddProductActivity;
+import com.lambton.lab2_namrata_c0853345_android.R;
+import com.lambton.lab2_namrata_c0853345_android.model.Products;
+
+import java.util.ArrayList;
+
+public class ProductDetailAdapter extends RecyclerView.Adapter<ProductDetailAdapter.ProductDetailHolder> {
+
+    private Context mContext;
+    ArrayList<Products> products;
+
+    public ProductDetailAdapter(Context mContext, ArrayList<Products> products) {
+        this.mContext = mContext;
+        this.products = products;
+    }
+
+    @NonNull
+    @Override
+    public ProductDetailHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.product_list_detailview,parent,false);
+        return new ProductDetailHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductDetailHolder holder, @SuppressLint("RecyclerView") int position) {
+
+
+       holder.prod_name.setText(products.get(position).getProduct_name());
+       holder.prod_desc.setText(products.get(position).getProduct_description());
+       holder.prod_price.setText(String.valueOf(products.get(position).getProduct_price()));
+        holder.edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(mContext,AddProductActivity.class);
+                i.putExtra("prod_id",products.get(position).getProduct_id());
+                i.putExtra("prod_name",products.get(position).getProduct_name());
+                i.putExtra("prod_desc",products.get(position).getProduct_description());
+                i.putExtra("prod_price",products.get(position).getProduct_price());
+                mContext.startActivity(i);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ProductDetailHolder extends RecyclerView.ViewHolder {
+        TextView prod_name,prod_desc,prod_price;
+        Button edit;
+        public ProductDetailHolder(View itemView) {
+            super(itemView);
+            prod_name = itemView.findViewById(R.id.prod_name);
+            prod_desc = itemView.findViewById(R.id.prod_desc);
+            prod_price = itemView.findViewById(R.id.prod_price);
+            edit = itemView.findViewById(R.id.edit);
+        }
+    }
+}
